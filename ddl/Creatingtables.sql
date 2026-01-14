df4c735f6496effc6da885a34037af26ee4f95a5
Quiana Roberts <quianaroberts@outlook.com>
2026-01-13 19:58:37 -0600

    4 - Creatingtables

---
+++ ddl/Creatingtables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix.titles1
+(
+title_id varchar (10),
+title varchar (200),
+title_type varchar (5),
+description varchar (2000),
+release_year integer,
+age_certification varchar (10),
+runtime smallint,
+genres varchar (30) ARRAY[10],
+seasons smallint,
+imdb_score decimal(2,1),
+imdb_votes integer
+);
+)
