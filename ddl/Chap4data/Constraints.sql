4d5888aa6d8bd32604c5b2294e65b5cbc57d40d9
Quiana Roberts <quianaroberts@outlook.com>
2026-01-16 17:54:34 -0600

    16 - constraints

--- ddl/Chap4data/Constraints.sql
+++ ddl/Chap4data/Constraints.sql
@@ -1,6 +1,20 @@
 /* Check Constraint */
-CREATE TABLE netflix.titles2
+CREATE TABLE netflix.titles7a
 (
 	release_year integer CHECK (release_year>2010)
 	);
-)
+------------------------------------------------------------------
+/* Unique Constraints */
+CREATE TABLE netflix.titles8a
+(
+title_id varchar (10) UNIQUE
+);
+
+CREATE TABLE mytable (
+	a integer,
+	b integer,
+	c integer,
+	UNIQUE (a, c)
+);
+-----------------------------------------
+
