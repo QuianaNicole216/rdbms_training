4689a8479baa21828b37345b90c0eb6a4c6d5b63
Quiana Roberts <quianaroberts@outlook.com>
2026-02-19 18:26:53 -0600

    #5 query_data

---
+++ Chapter-5-query-data/query_data.sql
@@ -0,0 +1,13 @@
+/* Query Data (SELECT)
+WHAT data we want and from WHERE to take it */
+
+SELECT * 
+FROM netflix.titles
+----------------------------------------------------
+/*
+SELECT title_id, title, title_type, description, release_year
+FROM netflix.titles;
+-------------------------------------------------------
+SELECT product_num, price * 1.05
+FROM product;
+*/
