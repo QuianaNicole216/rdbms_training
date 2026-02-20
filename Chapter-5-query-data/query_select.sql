d9b35a0d49142d79203c6e71b72bc1f862595833
Quiana Roberts <quianaroberts@outlook.com>
2026-02-19 18:52:49 -0600

    #5 query_select

---
+++ Chapter-5-query-data/query_select.sql
@@ -0,0 +1,14 @@
+/* Comments
+
+1. WHAT and WHY
+2. Mute a specific block of code
+
+--Single line: a sequence of characters beginning with double dashes and extending to the end of the line
+Section: mute a section using backslashes and teh asterick
+*/
+
+SELECT*
+FROM netflix.titles
+LIMIT 10
+
+*/
