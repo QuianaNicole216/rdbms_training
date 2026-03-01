6d75428986df9bf97829f59e390aa4d2ad7056ca
Quiana Roberts <quianaroberts@outlook.com>
2026-03-01 14:19:34 -0600

    #5 datatype_conversion

---
+++ Chapter-5-query-data/datatype_conversion.sql
@@ -0,0 +1,7 @@
+/* Data-Type Conversions (CAST) */
+------------------------------------------
+--SELECT (15 + CAST('12' AS INT)) as output_result;
+--SELECT CAST(60.1 AS varchar);
+--SELECT CAST(my_string as integer)
+--SELECT CAST ('2022-08-01' AS timestamp);
+--------------------------------------------
