10acf400128c27a5e6377bedf1604bbb12d80c0a
Quiana Roberts <quianaroberts@outlook.com>
2026-03-12 21:52:52 -0500

    #8 create_ecommerceproducts

---
+++ Chapter-8-loading-datatset/create_ecommerceproducts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ecommerce_schema.products
+(
+	product_id varchar(32) PRIMARY KEY,
+	product_categgory_name varchar(50),
+	product_price integer,
+	product_weight_g integer,
+	product_length_cm integer,
+	product_height_cm integer,
+	product_width_cm integer
+)
+
+
+	
